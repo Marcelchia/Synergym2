41b476690d0b62939da00346d4d648b5e4607ba6
“Marcelchia” <marcel.chiayl@gmail.com>
2020-06-09 21:58:25 +0800

    first commit

---
+++ app/javascript/components/app.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Form from './form'
+
+class App extends React.Component {
+
+render() {
+
+    return(
+        <div class = "form">
+            <p>Hi APP </p>
+            <Form/>
+
+        </div>);
+
+
+        }
+
+};
+
+
+export default App;
